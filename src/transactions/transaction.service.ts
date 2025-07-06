7920053f3c9dad60679a3813393c15d9b4292389
g.avanzato <gianluca.avanzato@googlemail.com>
2025-07-06 20:04:03 +0200

    removed comment

--- src/transactions/transaction.service.ts
+++ src/transactions/transaction.service.ts
@@ -68,85 +68,8 @@ export class TransactionService {
       throw new InternalServerErrorException('Transaktion fehlgeschlagen');
     }
   }
-  /*async createTransaction(dto: CreateTransactionDto) {
-    try {
-      const result = await this.prisma.$transaction(
-        async (tx: Prisma.TransactionClient) => {
-          const from = await tx.account.findUnique({
-            where: { iban: dto.fromIban },
-          });
-          if (!from)
-            throw new NotFoundException('Sender-Account nicht gefunden');
-          const to = await tx.account.findUnique({
-            where: { iban: dto.toIban },
-          });
-          if (!to)
-            throw new NotFoundException('Empfänger-Account nicht gefunden');
-
-          // 1. Kontosaldo & Limit prüfen
-          TransactionRulesService.ensureSufficientFundsAndLimit(
-            from.balance,
-            from.limit,
-            dto.amount,
-          );
-
-          // 2. AML-Regel prüfen
-          TransactionRulesService.ensureAmlCompliance(
-            dto.amount,
-            from.amlThreshold,
-          );
-
-          // 3. Tageslimit prüfen
-          await TransactionRulesService.ensureDailyLimitNotExceeded(
-            tx,
-            from.id,
-            dto.amount,
-            from.dailyLimit,
-          );
-
-          // Salden berechnen
-          const beforeFrom = from.balance;
-          const afterFrom = beforeFrom.minus(dto.amount);
-          const beforeTo = to.balance;
-          const afterTo = beforeTo.plus(dto.amount);
-
-          // Kontostände updaten
-          await tx.account.update({
-            where: { iban: dto.fromIban },
-            data: { balance: afterFrom },
-          });
-          await tx.account.update({
-            where: { iban: dto.toIban },
-            data: { balance: afterTo },
-          });
-
-          // Transaktion schreiben
-          return tx.transaction.create({
-            data: {
-              fromAccountId: from.id,
-              toAccountId: to.id,
-              amount: dto.amount,
-              purpose: dto.purpose,
-              status: 'SUCCESS',
-              balanceBeforeFrom: beforeFrom,
-              balanceAfterFrom: afterFrom,
-              balanceBeforeTo: beforeTo,
-              balanceAfterTo: afterTo,
-            },
-          });
-        },
-      );
-      return result;
-    } catch (error) {
-      if (
-        error instanceof NotFoundException ||
-        error instanceof BadRequestException
-      ) {
-        throw error;
-      }
-      throw new InternalServerErrorException(`Transaktion fehlgeschlagen`);
-    }
-  }
+  /*
+ 
 
   async findAll() {
     return this.prisma.transaction.findMany();
